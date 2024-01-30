8eaeff415d6af77e82d37eb1a25ac11f5b73cd60
shiqstone <shiqstone@gmail.com>
2024-01-31 00:58:17 +0800

    refactor

---
+++ biz/model/image/coordinate.go
@@ -0,0 +1,6 @@
+package image
+
+type Coordinate struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+}
--- biz/model/order/order.pb.go
+++ biz/model/order/order.pb.go
@@ -6,7 +6,8 @@ import (
 
 const (
 	STATUS_INIT    int16 = 0 //未处理
-	STATUS_SUCCESS int16 = 1 //成功
-	STATUS_FALID   int16 = 2 //失败
-	STATUS_UNKNOW  int16 = 3 //未知状态
+	STATUS_PAYED   int16 = 1 //已支付，待处理
+	STATUS_SUCCESS int16 = 2 //成功
+	STATUS_FALID   int16 = 3 //失败
+	STATUS_UNKNOW  int16 = 4 //未知状态
 )
